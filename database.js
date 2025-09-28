2f1a623af14687d310aab045e51b49996d811fa6
Richard Mas <richardmas@Richards-MacBook-Air.local>
2025-09-27 22:01:02 -0400

    Fix database connection

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
+        ssl: false,
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
